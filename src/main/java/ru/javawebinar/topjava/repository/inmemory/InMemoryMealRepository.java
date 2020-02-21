e778645df882f0c96c592386726d1c4238dd4a9d
Tropov Evgeny <tea-vspu@yandex.ru>
2020-02-21 23:04:20 +0300

    HW2_3

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,12 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -53,12 +52,18 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public Collection<Meal> getAll(int userId) {
+    public List<Meal> getAll(int userId) {
         log.info("get all for {}", userId);
-        return repository.get(userId).values().stream()
-                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
-                .collect(Collectors.toList());
+        return getByDate(userId, LocalDate.MIN, LocalDate.MAX);
     }
 
+    @Override
+    public List<Meal> getByDate(int userId, LocalDate begin, LocalDate end) {
+        log.info("get by date for {}", userId);
+        return repository.get(userId).values().stream()
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .filter(meal -> DateTimeUtil.isBetweenInclusive(meal.getDate(), begin, end))
+                .collect(Collectors.toList());
+    }
 }
 
